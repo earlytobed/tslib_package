9e54f57b278b88a4fa5c03c049d369bb2a20173b
Nighthawk <git@qaq.io>
2024-07-10 19:54:16 +0800

    chore: Update tslib_package submodule branch and dependencies in pyproject.toml
    chore: Update tslib_package submodule commit reference
    
    chore: Update test files and pre-commit configuration

---
+++ tests/test_data_provider.py
@@ -0,0 +1,2 @@
+def test_import_data_provider():
+    from tslib_package.data_provider.data_factory import data_provider  # noqa: F401
---
+++ tests/test_normalize_revin.py
@@ -0,0 +1,2 @@
+def test_import_normalize_revin():
+    from tslib_package.layers.StandardNorm import Normalize  # noqa: F401
